2d75270b2764684bbd56b5315e204f537486b883
mrunalk2398 <klkmrunal@gmail.com>
2024-10-03 17:44:40 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirement(file_path:str)-> List[str]:
+    reuirement = []
+    with open (file_path) as file_obj:
+        requirement = file_obj.readlines()
+        requirement = [req.replace('\n','') for req in requirement]
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+        return requirement
+
+
+setup(
+    name = 'Diamond_price_prediction',
+    version = '0.0.1',
+    author = 'Mrunal Kulkarni',
+    author_email = 'klkmrunal@gmail.com',
+    install_requires = [get_requirement('requirement.txt')],
+    packages = find_packages()
+)
